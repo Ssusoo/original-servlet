e69045800b03e135bb6f9de783ea1dd99369c20f
김수환 <zzanggoon8@hiconsy.com>
2023-06-09 12:47:53 +0900

    first commit

---
+++ src/test/java/me/ssu/originalservlet/OriginalServletApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.originalservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginalServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
